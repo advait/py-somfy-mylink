5c5d9f9179c44d8e30fbc8a4186f6fa9e5420244
Advait Shinde <advait.shinde@gmail.com>
2018-10-18 17:27:18 -0700

    Add documentation.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 import asyncio
 import json
 
-class SomfyMylink:
+class SomfyMyLink:
+    """Wraps access to a Somfy MyLink Hub via the Somfy Synergy API."""
+
     def __init__(self, system_id, host, port=44100):
         self.system_id = system_id
         self.host = host
@@ -10,7 +12,7 @@ class SomfyMylink:
         self._writer = self._reader = None
 
     async def _connect(self):
-        if self._writer and not self._writer.is_closing():
+        if self._writer and not self._writer.transport.is_closing():
             return
         self._reader, self._writer = await asyncio.open_connection(self.host, self.port)
 
@@ -45,19 +47,24 @@ class SomfyMylink:
         return await self._send('mylink.move.stop', target_id)
 
     def target(self, target_id):
-        return SomfyMylinkTarget(self, target_id)
+        """Returns a target that can be used to issue commands."""
+        return SomfyMyLinkTarget(self, target_id)
 
 
-class SomfyMylinkTarget:
+class SomfyMyLinkTarget:
+    """Represents a single Somfy MyLink device/target."""
     def __init__(self, somfy, target_id):
         self._somfy = somfy
         self._target_id = target_id
 
     async def up(self):
+        """Open the shade."""
         return await self._somfy._up(self._target_id)
 
     async def down(self):
+        """Close the shade."""
         return await self._somfy._down(self._target_id)
 
     async def stop(self):
+        """Stop the shade."""
         return await self._somfy._stop(self._target_id)
