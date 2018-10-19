2a0e40a984bc8c6677a36771146a44dfac416a01
Advait Shinde <advait.shinde@gmail.com>
2018-10-19 13:34:26 -0700

    Rev to 0.0.3.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="somfy_mylink",
-    version="0.0.2",
+    version="0.0.3",
     author="Advait Shinde",
     author_email="advait.shinde@gmail.com",
     description="Python API bindings for the Somfy MyLink Synergy API",
