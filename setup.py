4c7eb811c94cd8f4048a8dee37933cfe7c24c29e
Advait Shinde <advait.shinde@gmail.com>
2018-10-18 19:40:47 -0700

    Indicate MIT license in setuptools.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="somfy_mylink",
-    version="0.0.1",
+    version="0.0.2",
     author="Advait Shinde",
     author_email="advait.shinde@gmail.com",
     description="Python API bindings for the Somfy MyLink Synergy API",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/advait/py-somfy-mylink",
     packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
